5efe5ce567cf41aff4556fe6ec2a626e1e9f4a92
namvfg <duakhongchau1234@gmail.com>
2024-11-13 22:25:50 +0700

    load dc products

--- app/app/dao.py
+++ app/app/dao.py
@@ -5,3 +5,7 @@ from app import appdemo
 def load_categories():
     with open("%s/data/categories.json" % appdemo.root_path, encoding='utf-8') as f:
         return json.load(f)
+
+def load_products():
+    with open("%s/data/products.json" % appdemo.root_path, encoding='utf-8') as f:
+        return json.load(f)
--- app/app/index.py
+++ app/app/index.py
@@ -6,7 +6,10 @@ from app import appdemo
 @appdemo.route("/")
 def index():
     cates = dao.load_categories()
-    return render_template("index.html", categories = cates)
+    prods = dao.load_products()
+    return render_template("index.html",
+                           categories=cates,
+                           products=prods)
 
 if __name__ == "__main__":
     appdemo.run(debug=True)
