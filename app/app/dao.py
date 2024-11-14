96ad8f0ec36dc2527f0c448beb330aea35072f3d
namvfg <duakhongchau1234@gmail.com>
2024-11-14 15:05:02 +0700

    tim theo category_id

--- app/app/dao.py
+++ app/app/dao.py
@@ -6,6 +6,11 @@ def load_categories():
     with open("%s/data/categories.json" % appdemo.root_path, encoding='utf-8') as f:
         return json.load(f)
 
-def load_products():
+def load_products(cate_id=None):
     with open("%s/data/products.json" % appdemo.root_path, encoding='utf-8') as f:
-        return json.load(f)
+        products = json.load(f)
+
+    if cate_id:
+        products = [prod for prod in products if prod["category_id"] == int(cate_id)]
+
+    return products
--- app/app/index.py
+++ app/app/index.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from app import dao
 from app import appdemo
 
@@ -6,7 +6,9 @@ from app import appdemo
 @appdemo.route("/")
 def index():
     cates = dao.load_categories()
-    prods = dao.load_products()
+
+    cate_id = request.args.get("category_id")
+    prods = dao.load_products(cate_id)
     return render_template("index.html",
                            categories=cates,
                            products=prods)
