687c700c61764495db4ffde8c6e317d45c6fe3c8
namvfg <duakhongchau1234@gmail.com>
2024-11-17 14:03:06 +0700

    tach file html va tim kiem va chi tiet san pham

--- app/app/dao.py
+++ app/app/dao.py
@@ -1,14 +1,21 @@
+
 from app import appdemo
 from app.models import Category, Product
 
 
 def load_categories():
-    return Category.query.all()
+    return Category.query.order_by("id").all()
 
-def load_products(cate_id=None):
+def load_products(cate_id=None, keyword=None):
     products = Product.query
 
     if cate_id:
         products = products.filter(Product.category_id.__eq__(cate_id))
 
+    if keyword:
+        products = products.filter(Product.name.contains(keyword))
+
     return products.all()
+
+def get_product_by_id(product_id):
+    return Product.query.get(product_id)
--- app/app/index.py
+++ app/app/index.py
@@ -1,17 +1,24 @@
 from flask import render_template, request
 from app import dao
 from app import appdemo
+from app.dao import get_product_by_id
 
 
 @appdemo.route("/")
 def index():
-    cates = dao.load_categories()
-
     cate_id = request.args.get("category_id")
-    prods = dao.load_products(cate_id)
+    keyword = request.args.get("keyword")
+
+    cates = dao.load_categories()
+    prods = dao.load_products(cate_id=cate_id, keyword=keyword)
     return render_template("index.html",
                            categories=cates,
                            products=prods)
+
+@appdemo.route("/product/<int:product_id>")
+def details(product_id):
+    product = get_product_by_id(product_id=product_id)
+    return render_template("detail.html", product=product)
 
 if __name__ == "__main__":
     appdemo.run(debug=True)
